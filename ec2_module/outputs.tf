b697b2fbaf4adf5a5aa8d5bd04ae4625dd7d3749
vinothkumarkarunagaran <164986303+vinothkumarkarunagaran@users.noreply.github.com>
2025-11-29 13:57:04 +0530

    Update EC2 outputs to reference 'server' instance

--- ec2_module/outputs.tf
+++ ec2_module/outputs.tf
@@ -1,24 +1,24 @@
 output "instance_id" {
   description = "The ID of the EC2 instance"
-  value       = aws_instance.this.id
+  value       = aws_instance.server.id
 }
 
 output "public_ip" {
   description = "Public IP address (if assigned)"
-  value       = aws_instance.this.public_ip
+  value       = aws_instance.server.public_ip
 }
 
 output "public_dns" {
   description = "Public DNS name (if assigned)"
-  value       = aws_instance.this.public_dns
+  value       = aws_instance.server.public_dns
 }
 
 output "private_ip" {
   description = "Private IP address"
-  value       = aws_instance.this.private_ip
+  value       = aws_instance.server.private_ip
 }
 
 output "security_group_ids" {
   description = "Security group IDs attached to the instance"
-  value       = aws_instance.this.vpc_security_group_ids
+  value       = aws_instance.server.vpc_security_group_ids
 }
