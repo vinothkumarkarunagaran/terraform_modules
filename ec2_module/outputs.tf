c0bc132ee89df5f031ec3a6849742c040c686cf9
vinothkumarkarunagaran <164986303+vinothkumarkarunagaran@users.noreply.github.com>
2025-11-29 14:10:17 +0530

    Update outputs for multiple EC2 instances

--- ec2_module/outputs.tf
+++ ec2_module/outputs.tf
@@ -1,24 +1,29 @@
-output "instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = aws_instance.server.id
+output "instance_ids" {
+  description = "The IDs of the EC2 instances"
+  value       = aws_instance.server[*].id
 }
 
-output "public_ip" {
-  description = "Public IP address (if assigned)"
-  value       = aws_instance.server.public_ip
+output "public_ips" {
+  description = "Public IP addresses (if assigned)"
+  value       = aws_instance.server[*].public_ip
 }
 
 output "public_dns" {
-  description = "Public DNS name (if assigned)"
-  value       = aws_instance.server.public_dns
+  description = "Public DNS names (if assigned)"
+  value       = aws_instance.server[*].public_dns
 }
 
-output "private_ip" {
-  description = "Private IP address"
-  value       = aws_instance.server.private_ip
+output "private_ips" {
+  description = "Private IP addresses"
+  value       = aws_instance.server[*].private_ip
 }
 
 output "security_group_ids" {
-  description = "Security group IDs attached to the instance"
-  value       = aws_instance.server.vpc_security_group_ids
+  description = "Security group IDs attached to the instances"
+  value       = aws_instance.server[*].vpc_security_group_ids
+}
+
+output "instance_role_tag" {
+  description = "Role tag value applied to instances"
+  value       = var.tag
 }
