0a690ad2057b96aaef7f2e0ce557f82bbce60636
vinothkumarkarunagaran <164986303+vinothkumarkarunagaran@users.noreply.github.com>
2025-11-29 13:50:29 +0530

    Refactor EC2 instance resource configuration

--- ec2_module/main.tf
+++ ec2_module/main.tf
@@ -8,35 +8,27 @@ terraform {
 	}
 }
 
-locals {
-	merged_tags = merge({ Name = var.name }, var.tags)
-	sg_ids = length(var.vpc_security_group_ids) > 0 ? var.vpc_security_group_ids : aws_security_group.this[*].id
-}
-
-
-resource "aws_instance" "name" {
+resource "aws_instance" "server" {
   count = var.instance_count
 
   ami                         = var.ami
   instance_type               = var.instance_type
   key_name                    = var.key_name
   associate_public_ip_address = var.associate_public_ip_address
-  vpc_security_group_ids      = var.security_group_ids
+  vpc_security_group_ids      = var.vpc_security_group_ids   # ✅ DIRECT VARIABLE
 
+  dynamic "ebs_block_device" {
+    for_each = var.ebs_volumes
+    content {
+      device_name           = ebs_block_device.value.device_name
+      volume_size           = ebs_block_device.value.size
+      volume_type           = ebs_block_device.value.type
+      delete_on_termination = try(ebs_block_device.value.delete_on_termination, true)
+    }
+  }
 
+  tags = merge(
+    { Name = "server-${count.index + 1}" },
+    var.tags
+  )
 }
-
-dynamic "ebs_block_device" {
-	for_each = var.ebs_volumes
-	content {
-		device_name           = ebs_block_device.value.device_name
-		volume_size           = ebs_block_device.value.size
-		volume_type           = ebs_block_device.value.type
-		delete_on_termination = try(ebs_block_device.value.delete_on_termination, true)
-	}
-}
-
-tags = {
-Name = "server-${count.index + 1}"
-}
-
